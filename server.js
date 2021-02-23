c3600d6c9b754b4bc8e2316b8d2e0e9a796f5fab
MWPX777 <mwpx777@gmail.com>
2021-02-23 11:50:35 -0600

    add Heroku

--- server.js
+++ server.js
@@ -1,54 +1,46 @@
+// dependencies
 const express = require('express');
 const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
 const notes = require('./Develop/db/db');
-// const db = require('./Develop/db/db')
 
-// middleware
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
-
-// require('./routes/apiRoutes');
-// require('./routes/htmlRoutes');
-
-app.get('/api/db', (req, res) => {
-  
-    // res.send("this is working now!")
-});
-
 // display notes from db.json
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-    // res.send("come on please work")
+    // res.send("notes endpoint working")
     // res.sendFile(path.join(__dirname, './Develop/db/db'))
+    
 });
 
 // add new notes to db.json
 app.post('/api/notes', (req, res) => {
-  
-        const note = JSON.parse(fs.readFileSync('./Develop/db/db'));
-        const newNote = require.body;
-        newNote.id = 
-        notesArray.push(newNote);
-        fs.writeFileSync(
-            path.join(__dirname, '..Develop/db/db.json'),
-            JSON.stringify({notesArray} , null, 2)
-        );
-        
-    });
 
-
-
-app.delete('/api/notes/:id' , (req, res) => {
+    const note = JSON.parse(fs.readFileSync('./Develop/db/db'));
+    const newNote = require.body;
+    newNote.id =
+        noteListItems.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, '..Develop/db/db.json'),
+        JSON.stringify({ noteListItems }, null, 2)
+    );
 
 });
 
+
+// delete note from db
+app.delete('/api/notes/:id', (req, res) => {
+
+});
+
+// port listener
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });

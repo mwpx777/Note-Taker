be5afc37a0b69ceba649d16c4ff9590bd730bcfe
MWPX777 <mwpx777@gmail.com>
2021-02-24 09:12:33 -0600

    everything working excepte delete note

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const { notes } = require('./Develop/db/db.json')
+// const uuidv4 = require("uuid");
+// const shortid = require('shortid');
 
 
 
 // Middleware
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+
 
 
 // display notes from db.json
@@ -35,29 +38,35 @@ function createNewNote(body, notesArray) {
 
 // add new notes to db.json
 app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-
-    // console.log(req.body);
+    req.body.id = notes.length.toString();   
+    
     const newNote = createNewNote(req.body, notes);
     // res.json(req.body)
     res.json(newNote);
 
 
 });
+
+
 // this displays index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 
 });
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 
 });
 
 // delete note from db
-// app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json'));
+    const deleteNote = notes.filter((removeNote) => removeNote.id !== req.params.id);
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(deleteNote));
+    res.json(deleteNote);
+});
 
-// });
+
 
 // port listener
 app.listen(PORT, () => {

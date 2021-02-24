c0fe35382d07a2bf7942ea9d4ba009483576f16b
MWPX777 <mwpx777@gmail.com>
2021-02-24 10:41:34 -0600

    fixed note text not displaying

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 // dependencies
+const { text } = require('express');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
 const { notes } = require('./Develop/db/db.json')
-// const uuidv4 = require("uuid");
-// const shortid = require('shortid');
-
-
 
 // Middleware
 app.use(express.static('public'));
@@ -16,12 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-
 // display notes from db.json
 app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/db/db.json'))
     res.json(notes);
-    // res.send("notes endpoint working")
-    // res.sendFile(path.join(__dirname, './Develop/db/db'))
 
 });
 
@@ -31,44 +26,40 @@ function createNewNote(body, notesArray) {
     fs.writeFileSync(
         path.join(__dirname, './Develop/db/db.json'),
         JSON.stringify({ notes: notesArray }, null, 2)
+        
     );
-    // console.log(body);
+
     return note;
-}
+};
 
 // add new notes to db.json
 app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();   
-    
+    // creates new id number based on array length
+    req.body.id = notes.length.toString();
     const newNote = createNewNote(req.body, notes);
-    // res.json(req.body)
     res.json(newNote);
-
-
 });
 
-
-// this displays index.html
+// display index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-
 });
+
+// display notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-
 });
 
 // delete note from db
 app.delete('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
     const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json'));
-    const deleteNote = notes.filter((removeNote) => removeNote.id !== req.params.id);
-    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(deleteNote));
-    res.json(deleteNote);
+    const removeNote = notes.filter((deleteNote) => deleteNote.id !== req.params.id);
+    fs.writeFileSync('./Develop/db/db.json', JSON.stringify(removeNote));
+    res.json(removeNote);
 });
-
-
 
 // port listener
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on port http://localhost:${PORT}!`);
 });
